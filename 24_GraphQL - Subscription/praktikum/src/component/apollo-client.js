1b0624572c388be73478fcef88ffa977f6c4af93
Jamaluddin <76935070+jamaluddin2000@users.noreply.github.com>
2022-04-25 14:13:20 +0800

    upload section 24

---
+++ 24_GraphQL - Subscription/praktikum/src/component/apollo-client.js
@@ -0,0 +1,47 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
+
+const httpLink = new HttpLink({
+  uri: 'https://real-polecat-61.hasura.app/v1/graphql',
+  headers : {
+    'x-hasura-admin-secret' : 'tC745Kuj3icUyzOWAgZGsq4kxtrpORGZNgVg53haXGVLUG57VKspGS95GS7GX6vV'
+},
+});
+
+const wsLink = new GraphQLWsLink(createClient({
+    url: 'wss://real-polecat-61.hasura.app/v1/graphql',
+    connectionParams: {
+        headers : {
+            'x-hasura-admin-secret' : 'tC745Kuj3icUyzOWAgZGsq4kxtrpORGZNgVg53haXGVLUG57VKspGS95GS7GX6vV'
+        },
+    },
+  }));
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+
+
+const client = new ApolloClient({
+    link: splitLink,
+    cache: new InMemoryCache(),
+});
+
+export default client;
